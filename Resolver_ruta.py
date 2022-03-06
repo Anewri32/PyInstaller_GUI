00ec41588c3b4b03255f87eb9e6847b5b2a8eb1e
Anewri32 <70771409+anewri32@users.noreply.github.com>
2022-03-06 18:40:41 -0400

    Funcion que resuelve rutas

---
+++ Resolver_ruta.py
@@ -0,0 +1,6 @@
+import os, sys
+
+def resolver_ruta(ruta_relativa):
+    if hasattr(sys, '_MEIPASS'):
+        return os.path.join(sys._MEIPASS, ruta_relativa)
+    return os.path.join(os.path.abspath('.'), ruta_relativa)
